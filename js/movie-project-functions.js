4755fc3ba7a498d59a4efebe76b3d7c29393233a
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-04-03 14:57:07 -0500

    adding the other database

--- js/movie-project-functions.js
+++ js/movie-project-functions.js
@@ -1,12 +1,25 @@
-import {OMDB_API} from "./keys.js"
+import {OMDB_API, TMDB_API} from "./keys.js"
 
-export async function movieData() {
+// Pull movie data from API for one particular movie
+// export async function movieData() {
+//     try {
+//         const response = await fetch(`http://www.omdbapi.com/?i=tt0068646&apikey=${OMDB_API}`);
+//         const data = await response.json();
+//         return data;
+//     } catch(error) {
+//         console.log(error);
+//     }
+// }
+
+
+export async function tmdbMovieData  () {
     try {
-        const response = await fetch(`http://www.omdbapi.com/?i=tt0068646&apikey=${OMDB_API}`);
+        const response = await fetch (`https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API}&language=en-US&page=1`);
         const data = await response.json();
         return data;
     } catch(error) {
-        console.log(error);
+        console.log (error)
     }
 }
+
 
