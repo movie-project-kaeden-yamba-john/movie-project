8109576651afee57312bc2d73ac321e9d8e59f64
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-04-04 09:52:03 -0500

    Adding add create delete update functions

--- js/movie-project-functions.js
+++ js/movie-project-functions.js
@@ -1,102 +1,134 @@
-import {TMDB_API} from "./keys.js"
+// import {TMDB_API} from "./keys.js"
+//
+// // Fetches data from TMDB for popular movies on a specific date (today)
+// export async function tmdbMovieData  () {
+//     try {
+//         const response = await fetch (`https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API}&language=en-US&page=1`);
+//         const data = await response.json();
+//         return data;
+//     } catch(error) {
+//         console.log (error)
+//     }
+// }
+//
+// // Pulls only the results, array of movies, from the data above
+// export async function getMovieArray () {
+//     try {
+//         const response = await fetch (`https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API}&language=en-US&page=1`);
+//         const data = await response.json();
+//         return data.results;
+//     } catch (error) {
+//         return error
+//     }
+// }
 
-// Fetches data from TMDB for popular movies on a specific date (today)
-export async function tmdbMovieData  () {
+// Rendering Movies from TMDB Database
+// export const renderMovie = (movie, parent) => {
+//     const element = document.createElement('div');
+//     element.classList.add(`movie-card`);
+//     element.innerHTML = `
+//     <div class="column">
+//         <div class="img-wrapper">
+//             <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="movie poster" class="moviePoster">
+//         </div>
+//     </div>
+//     <div class="column">
+//          <div class="movie-info">
+//             <h2>${movie.title}</h2>
+//             <p>${movie.overview}</p>
+//             <button>Delete Movie</button>
+//         </div>
+//     </div>
+//     `;
+//     document.getElementById('movieCard').appendChild(element)
+// }
+
+
+
+
+// Function to get all movies from the database
+export const getMovies = async () => {
     try {
-        const response = await fetch (`https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API}&language=en-US&page=1`);
+        const response = await fetch('http://localhost:3000/movies');
         const data = await response.json();
+        console.log('Movies:', data);
         return data;
-    } catch(error) {
-        console.log (error)
-    }
-}
-
-// Pulls only the results, array of movies, from the data above
-export async function getMovieArray () {
-    try {
-        const response = await fetch (`https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API}&language=en-US&page=1`);
-        const data = await response.json();
-        return data.results;
     } catch (error) {
-        return error
+        console.error('Error getting movies:', error);
     }
-}
+};
 
-// Function to display movies on the page
-export function displayMovies(movies) {
+//Function to add a new movie to the database
+export const addMovie = async (title, rating, genre) => {
+    try {
+        const response = await fetch('http://localhost:3000/movies/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title, rating, genre })
+        });
+        const data = await response.json();
+        console.log('Movie added:', data);
+        return data;
+    } catch (error) {
+        console.error('Error adding movie:', error);
+    }
+};
+
+// Function to update an existing movie in the database
+export const updateMovie = async (id, title, rating, genre) => {
+    try {
+        const response = await fetch(`http://localhost:3000/movies/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title, rating, genre })
+        });
+        const data = await response.json();
+        console.log('Movie updated:', data);
+        return data;
+    } catch (error) {
+        console.error('Error updating movie:', error);
+    }
+};
+
+// Function to delete an existing movie from the database
+export const deleteMovie = async (id) => {
+    try {
+        const response = await fetch(`http://localhost:3000/movies/${id}`, {
+            method: 'DELETE'
+        });
+        const data = await response.json();
+        console.log('Movie deleted:', data);
+        return data;
+    } catch (error) {
+        console.error('Error deleting movie:', error);
+    }
+};
+
+//function to search for a movie
+export const searchMovies = async (searchText) => {
+    try {
+        const response = await fetch(`http://localhost:3000/movies?q=${searchText}`);
+        const data = await response.json();
+        console.log('Movies:', data);
+        displayMovies(data);
+    } catch (error) {
+        console.error('Error searching movies:', error);
+    }
+};
+
+// Function to display all movies in the HTML page
+export const displayMovies = (movies) => {
+    const movieList = document.getElementById('movie-list');
     movieList.innerHTML = '';
-
-    movies.forEach(movie => {
-        const movieDiv = document.createElement('div');
-        movieDiv.innerHTML = `
-      <h3>${movie.title} (${movie.rating})</h3>
-      <p>Genre: ${movie.genre}</p>
-      <button class="edit-button" data-id="${movie.id}">Edit</button>
-      <button class="delete-button" data-id="${movie.id}">Delete</button>
-    `;
-        movieList.appendChild(movieDiv);
+    movies.forEach((movie) => {
+        const movieItem = document.createElement('div');
+        movieItem.classList.add('movie-item');
+        movieItem.innerHTML = `
+      <h2>${movie.title}</h2>
+      <p><strong>Rating:</strong> ${movie.rating}</p>
+      <p><strong>Genre:</strong> ${movie.genre}</p>
+      <button class="edit-movie-btn" data-id="${movie.id}">Edit</button>
+      <button class="delete-movie-btn" data-id="${movie.id}">Delete</button>`;
+        movieList.appendChild(movieItem);
     });
-}
-
-// Function to add a movie to the database
-export function addMovie(title, rating, genre) {
-    fetch('http://localhost:3000/movies/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title, rating, genre })
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Movie added:', data);
-            displayMovies(data.movies);
-        })
-        .catch(error => {
-            console.error('Error adding movie:', error);
-        });
-}
-
-// Function to remove a movie from the database
-export function removeMovie(id) {
-    fetch(` http://localhost:3000/movies/${id}`, {
-        method: 'DELETE'
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Movie removed:', data);
-            let movies =
-                displayMovies(data.movies);
-        })
-        .catch(error => {
-            console.error('Error removing movie:', error);
-        });
-}
-
-// Function to update a movie in the database
-export function updateMovie(id, title, rating, genre) {
-    fetch(` http://localhost:3000/movies/${id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title, rating, genre })
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Movie updated:', data);
-            displayMovies(data.movies);
-        })
-        .catch(error => {
-            console.error('Error updating movie:', error);
-        });
-}
-
-// Fetch the list of movies from the database and display them on the page
-export function displayMovie() {
-    fetch(' http://localhost:3000/movies/')
-        .then(response => response.json())
-        .then(data => {
-            console.log('Movies:', data.movies);
-            displayMovies(data.movies);
-        })
-        .catch(error => {
-            console.error('Error fetching movies:', error);
-        });
-}
+};
--- js/movie-project-main.js
+++ js/movie-project-main.js
@@ -1,52 +1,134 @@
-import * as movieMain from "./movie-project-functions.js";
-import {displayMovies} from "./movie-project-functions.js";
-import {addMovie} from "./movie-project-functions.js";
-import {removeMovie} from "./movie-project-functions.js";
-import {updateMovie} from "./movie-project-functions.js";
-import {displayMovie} from "./movie-project-functions.js";
+// import * as movieMain from "./movie-project-functions.js";
+//
+// (async () => {
+//     // Pulls an array of movies, with each being objects, from  the TMDB Database;
+//     let tmdbMovieData = await movieMain.tmdbMovieData();
+//     console.log(tmdbMovieData)
+//     let moviesArray = await movieMain.getMovieArray();
+//     console.log(moviesArray)
+//     const movieGrid = document.querySelector('#movieGrid')
+//     moviesArray.forEach(function(movie){
+//         movieMain.renderMovie(movie, movieGrid);
+//     });
+// })();
+// const movieList = document.getElementById('movie-list');
+// const addMovieForm = document.getElementById('add-movie-form');
+//
+// movieMain.displayMovies()
+//
+// // Add an event listener to the form to add a new movie
+// addMovieForm.addEventListener('submit', event => {
+//     event.preventDefault();
+//
+//     const title = document.getElementById('title').value;
+//     const rating = document.getElementById('rating').value;
+//     const genre = document.getElementById('genre').value;
+//
+//     movieMain.addMovie(title, rating, genre);
+//
+//     addMovieForm.reset();
+// });
+//
+// // Add event listeners to the movie list for editing and deleting movies
+// movieList.addEventListener('click', event => {
+//     const target = event.target;
+//
+//     if (target.classList.contains('edit-button')) {
+//         const id = target.dataset.id;
+//         const title = prompt('Enter the new title:');
+//         const rating = prompt('Enter the new rating:');
+//         const genre = prompt('Enter the new genre:');
+//         movieMain.updateMovie(id, title, rating, genre);
+//     } else if (target.classList.contains('delete-button')) {
+//         const id = target.dataset.id;
+//         const confirmDelete = confirm('Are you sure you want to delete this movie?');
+//         if (confirmDelete) {
+//             movieMain.removeMovie(id);
+//         }
+//     }
+// });
+//
+// movieMain.displayMovie()
 
-(async () => {
-    // Pulls an array of movies, with each being objects, from  the TMDB Database;
-    let tmdbMovieData = await movieMain.tmdbMovieData();
-    console.log(tmdbMovieData)
-    let movieDataArray = await movieMain.getMovieArray();
-    console.log(movieDataArray)
 
-})();
-const movieList = document.getElementById('movie-list');
-const addMovieForm = document.getElementById('add-movie-form');
 
-displayMovies()
-// Add an event listener to the form to add a new movie
-addMovieForm.addEventListener('submit', event => {
+import { getMovies, addMovie, updateMovie, deleteMovie, searchMovies } from './movie-project-functions.js';
+
+const movieList = document.querySelector('#movie-list');
+const addMovieForm = document.querySelector('#add-movie-form');
+const searchForm = document.querySelector('#search-form');
+
+// Function to display movies in the movie list
+const displayMovies = (movies) => {
+    movieList.innerHTML = '';
+    movies.forEach(movie => {
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('movie');
+        movieElement.innerHTML = `
+      <h3>${movie.title}</h3>
+      <p>Rating: ${movie.rating}</p>
+      <p>Genre: ${movie.genre}</p>
+      <button class="delete-button" data-id="${movie.id}">Delete</button>
+      <button class="edit-button" data-id="${movie.id}">Edit</button>
+    `;
+        movieList.appendChild(movieElement);
+    });
+}
+
+// Function to handle adding a new movie
+const handleAddMovie = async (event) => {
     event.preventDefault();
-
-    const title = document.getElementById('title').value;
-    const rating = document.getElementById('rating').value;
-    const genre = document.getElementById('genre').value;
-
-    movieMain.addMovie(title, rating, genre);
-
+    const title = addMovieForm.elements.title.value;
+    const rating = addMovieForm.elements.rating.value;
+    const genre = addMovieForm.elements.genre.value;
+    await addMovie(title, rating, genre);
     addMovieForm.reset();
-});
+}
 
-// Add event listeners to the movie list for editing and deleting movies
+// Function to handle updating a movie
+const handleUpdateMovie = async (event) => {
+    const movieId = event.target.dataset.id;
+    const title = prompt('Enter new movie title:');
+    const rating = prompt('Enter new movie rating:');
+    const genre = prompt('Enter new movie genre:');
+    await updateMovie(movieId, title, rating, genre);
+    const movies = await getMovies();
+    displayMovies(movies);
+}
+
+// Function to handle deleting a movie
+const handleDeleteMovie = async (event) => {
+    const movieId = event.target.dataset.id;
+    const confirmation = confirm('Are you sure you want to delete this movie?');
+    if (confirmation) {
+        await deleteMovie(movieId);
+        const movies = await getMovies();
+        displayMovies(movies);
+    }
+}
+
+// Function to handle searching for movies
+// const handleSearchMovies = async (event) => {
+//     event.preventDefault();
+//     const searchQuery = searchForm.elements.search.value;
+//     const searchType = searchForm.elements.searchType.value;
+//     const movies = await searchMovies(searchQuery, searchType);
+//     displayMovies(movies);
+// }
+
+// Add event listeners
+addMovieForm.addEventListener('submit', handleAddMovie);
 movieList.addEventListener('click', event => {
-    const target = event.target;
-
-    if (target.classList.contains('edit-button')) {
-        const id = target.dataset.id;
-        const title = prompt('Enter the new title:');
-        const rating = prompt('Enter the new rating:');
-        const genre = prompt('Enter the new genre:');
-        movieMain.updateMovie(id, title, rating, genre);
-    } else if (target.classList.contains('delete-button')) {
-        const id = target.dataset.id;
-        const confirmDelete = confirm('Are you sure you want to delete this movie?');
-        if (confirmDelete) {
-            movieMain.removeMovie(id);
-        }
+    if (event.target.classList.contains('edit-button')) {
+        handleUpdateMovie(event);
+    } else if (event.target.classList.contains('delete-button')) {
+        handleDeleteMovie(event);
     }
 });
+// searchForm.addEventListener('submit', handleSearchMovies);
 
-displayMovie()
+// Display all movies on page load
+(async () => {
+    const movies = await getMovies();
+    displayMovies(movies);
+})();
