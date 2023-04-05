856c08884afeaf7edd42c4463145e16ec5c43a0c
John Buhr <Johnbuhr92@gmail.com>
2023-04-05 13:33:12 -0500

    final commit

--- js/movie-project-main.js
+++ js/movie-project-main.js
@@ -1,56 +1,3 @@
-// import * as movieMain from "./movie-project-functions.js";
-//
-// (async () => {
-//     // Pulls an array of movies, with each being objects, from  the TMDB Database;
-//     let tmdbMovieData = await movieMain.tmdbMovieData();
-//     console.log(tmdbMovieData)
-//     let moviesArray = await movieMain.getMovieArray();
-//     console.log(moviesArray)
-//     const movieGrid = document.querySelector('#movieGrid')
-//     moviesArray.forEach(function(movie){
-//         movieMain.renderMovie(movie, movieGrid);
-//     });
-// })();
-// const movieList = document.getElementById('movie-list');
-// const addMovieForm = document.getElementById('add-movie-form');
-//
-// movieMain.displayMovies()
-//
-// // Add an event listener to the form to add a new movie
-// addMovieForm.addEventListener('submit', event => {
-//     event.preventDefault();
-//
-//     const title = document.getElementById('title').value;
-//     const rating = document.getElementById('rating').value;
-//     const genre = document.getElementById('genre').value;
-//
-//     movieMain.addMovie(title, rating, genre);
-//
-//     addMovieForm.reset();
-// });
-//
-// // Add event listeners to the movie list for editing and deleting movies
-// movieList.addEventListener('click', event => {
-//     const target = event.target;
-//
-//     if (target.classList.contains('edit-button')) {
-//         const id = target.dataset.id;
-//         const title = prompt('Enter the new title:');
-//         const rating = prompt('Enter the new rating:');
-//         const genre = prompt('Enter the new genre:');
-//         movieMain.updateMovie(id, title, rating, genre);
-//     } else if (target.classList.contains('delete-button')) {
-//         const id = target.dataset.id;
-//         const confirmDelete = confirm('Are you sure you want to delete this movie?');
-//         if (confirmDelete) {
-//             movieMain.removeMovie(id);
-//         }
-//     }
-// });
-//
-// movieMain.displayMovie()
-
-
 import { getMovies, addMovie, updateMovie, deleteMovie, searchMovies } from './movie-project-functions.js';
 
 const movieList = document.querySelector('#movie-list');
@@ -82,6 +29,8 @@ const handleAddMovie = async (event) => {
     const genre = addMovieForm.elements.genre.value;
     await addMovie(title, rating, genre);
     addMovieForm.reset();
+    const movies = await getMovies();
+    displayMovies(movies);
 }
 
 // Function to handle updating a movie
@@ -106,15 +55,6 @@ const handleDeleteMovie = async (event) => {
     }
 }
 
-// Function to handle searching for movies
-// const handleSearchMovies = async (event) => {
-//     event.preventDefault();
-//     const searchQuery = searchForm.elements.search.value;
-//     const searchType = searchForm.elements.searchType.value;
-//     const movies = await searchMovies(searchQuery, searchType);
-//     displayMovies(movies);
-// }
-
 // Add event listeners
 addMovieForm.addEventListener('submit', handleAddMovie);
 movieList.addEventListener('click', event => {
@@ -124,7 +64,6 @@ movieList.addEventListener('click', event => {
         handleDeleteMovie(event);
     }
 });
-// searchForm.addEventListener('submit', handleSearchMovies);
 
 // Display all movies on page load
 (async () => {
